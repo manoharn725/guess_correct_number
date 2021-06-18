5be89b55260c6e74b0a5e173b2a916fa6e7fbb61
manohar <manoharnmanu725@gmail.com>
2021-06-18 23:38:00 +0530

    guess number

---
+++ guess_correct_number.py
@@ -0,0 +1,28 @@
+import random
+
+randNumber = random.randint(1,100)
+print(randNumber)
+
+userGuess=None
+guesses=0
+while(userGuess != randNumber):
+    userGuess= int(input(f"userGuess the random number: "))
+    guesses +=1 
+    if(userGuess==randNumber):
+        print("congrats you gussed right number!!!")
+    else:
+        if(userGuess<randNumber):
+            print("sorry you guessed wrong number!!! please enter the larger number")
+        else:
+            print("sorry you guessed wrong numbber!!! please enter the smaller  number ")  
+
+
+print(f"you guessed number in {guesses} guesses")             
+
+with open ("highest.txt","r")as f:
+    highest= int(f.read())
+
+if(highest>guesses):
+    print("you have braked last highestScore Record!!!")
+    with open ("highest.txt","w")as f:
+        f.write(str(guesses))
